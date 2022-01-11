ec47dfc336bc6bf524e887d6a5df5a58dc6c6f68
Eder Soares <edersoares@me.com>
2022-01-11 01:27:53 -0300

    Vite Plugin Assets

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { PluginOption } from 'vite';
+
+type AssetsPluginOptions = {
+  replaces: string[];
+};
+
+const getOrigin = (server): string => {
+  const host = server.host || 'localhost';
+  const port = server.port;
+  const protocol = server.https ? 'https' : 'http';
+
+  return `${protocol}://${host}:${port}`;
+};
+
+const replace = (code: string, replaces: string[], server): string => {
+  const origin = getOrigin(server);
+
+  replaces.forEach((replace: string) => {
+    const regex = new RegExp(`${replace}(.*)`, 'g');
+    code = code.replace(regex, `${origin}${replace}$1`);
+  });
+
+  return code;
+};
+
+export default function assets(options: AssetsPluginOptions): PluginOption {
+  let server;
+  return {
+    name: 'assets',
+    apply: 'serve',
+    enforce: 'post',
+    configureServer(vite) {
+      server = vite.config.server;
+    },
+    transform: (code: string) => ({
+      code: replace(code, options.replaces, server),
+      map: null,
+    }),
+    transformIndexHtml: (html: string) =>
+      replace(html, options.replaces, server),
+  };
+}
