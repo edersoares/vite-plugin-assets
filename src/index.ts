46d74deee1c25235c328dbf73d337c73420e8e5b
Eder Soares <edersoares@me.com>
2022-01-11 14:22:53 -0300

    Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { PluginOption } from 'vite';
+import { PluginOption } from "vite";
 
 type AssetsPluginOptions = {
   replaces: string[];
 };
 
 const getOrigin = (server): string => {
-  const host = server.host || 'localhost';
+  const host = server.host || "localhost";
   const port = server.port;
-  const protocol = server.https ? 'https' : 'http';
+  const protocol = server.https ? "https" : "http";
 
   return `${protocol}://${host}:${port}`;
 };
@@ -16,7 +16,7 @@ const replace = (code: string, replaces: string[], server): string => {
   const origin = getOrigin(server);
 
   replaces.forEach((replace: string) => {
-    const regex = new RegExp(`${replace}(.*)`, 'g');
+    const regex = new RegExp(`${replace}(.*)`, "g");
     code = code.replace(regex, `${origin}${replace}$1`);
   });
 
@@ -26,9 +26,9 @@ const replace = (code: string, replaces: string[], server): string => {
 export default function assets(options: AssetsPluginOptions): PluginOption {
   let server;
   return {
-    name: 'assets',
-    apply: 'serve',
-    enforce: 'post',
+    name: "assets",
+    apply: "serve",
+    enforce: "post",
     configureServer(vite) {
       server = vite.config.server;
     },
