1c12c1afd27d2e7917bd41e961055c7826f04137
Eder Soares <edersoares@me.com>
2022-01-15 19:19:28 -0300

    First real test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,3 +1,7 @@
-test('should be true', () => {
-  expect(true).toBe(true);
+import { getOrigin } from '../src';
+
+test('should return http://localhost', () => {
+  const origin = getOrigin({});
+
+  expect(origin).toBe('http://localhost');
 });
